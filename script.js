d358d70e269b2689c8098f005b7e92e88c12ef91
Louis <karpx033@umn.edu>
2021-12-20 22:28:35 -0600

    scores can be console logged and question moves to next in carousel

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var op1 =document.getElementById("op1");
 var op2 =document.getElementById("op2");
 var op3 =document.getElementById("op3");
 var op4 =document.getElementById("op4");
+var currentquestion=0
+var wins = 0;
+var losses = 0;
 
 var initialtime = 5;
 var testobj =  [
@@ -15,7 +18,7 @@ var testobj =  [
     a2: "Action peforming code",
     a3: "Elongated name for Java",
     a4: "Not this one",
-    correct: "a2"
+    correct: "B"
 },
 {
     question: "What is css",
@@ -23,7 +26,7 @@ var testobj =  [
     a2: "Cookies- Sugary, Sweet",
     a3: "Colors, scripts, sexbox",
     a4: "Not this one",
-    correct: "a1"
+    correct: "A"
 }
 ]
 
@@ -45,7 +48,6 @@ function startTimer() {
 
 function displayquestions() {
    console.log(test);
-    var currentquestion=0
     var qscreen = testobj[currentquestion];
     test.textContent= qscreen.question;
     op1.textContent=qscreen.a1;
@@ -55,8 +57,33 @@ function displayquestions() {
     
 };
 
+function updateScore() {
+    currentquestion++;
+    losses++;
+};
+
+function guesscheck(answer) {
+    if (testobj[currentquestion].correct == answer) {
+        wins++;
+        currentquestion++;
+        initialtime=5;
+        console.log(wins);
+        console.log(losses);
+        startTimer();
+        displayquestions();
+    } else {
+        losses++;
+        currentquestion++;
+        console.log(wins);
+        console.log(losses);
+        startTimer();
+        displayquestions();
+    }
+};
+
 
 start.addEventListener("click", function() {
     startTimer();
     displayquestions();
 });
+
