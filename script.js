33b23ed93b9fe4cbc5081dc60913c9306d48371e
Louis <karpx033@umn.edu>
2021-12-21 18:15:26 -0600

    now displaying tallying score and added a submit form

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ var op2 =document.getElementById("op2");
 var op3 =document.getElementById("op3");
 var op4 =document.getElementById("op4");
 var currentquestion=0
-var wins = 0;
-var losses = 0;
 var rw = document.getElementById("answeralert");
+var newform = document.createElement("form");
 
 var initialtime = 5;
 var testobj =  [
@@ -28,15 +27,40 @@ var testobj =  [
     a3: "Colors, scripts, sexbox",
     a4: "Not this one",
     correct: "A"
-}
+},
+{
+    question: "question 3",
+    a1: "wrong",
+    a2: "wrong",
+    a3: "wrong",
+    a4: "correct",
+    correct: "D"
+},
+{
+    question: "What is css",
+    a1: "wrong",
+    a2: "correct",
+    a3: "wrong",
+    a4: "wrong",
+    correct: "B"
+},
+{
+    question: "Enter your initials to see highscores",
+    a1: "",
+    a2: "",
+    a3: "",
+    a4: "",
+    correct: ""
+
+},
 ]
 
 console.log(timerstart);
 
 function startTimer() {
+        localStorage.score=0;
          var timerInterval = setInterval(function() {
             initialtime--;
-            console.log(initialtime);
             timerstart.textContent=initialtime
 
             if ( initialtime === 0) {
@@ -55,34 +79,31 @@ function displayquestions() {
     op2.textContent=qscreen.a2;
     op3.textContent=qscreen.a3;
     op4.textContent=qscreen.a4;
+
+    if (testobj.length == currentquestion+1) {
+
+        document.getElementById("scorecount").innerHTML= localStorage.score;
+        finalprompt();
+    }
+
     
 };
 
 function updateScore() {
     currentquestion++;
-    losses++;
 };
 
 function guesscheck(answer) {
     if (testobj[currentquestion].correct == answer) {
-        wins++;
         currentquestion++;
-        console.log(wins);
-        console.log(losses);
-        var score= initialtime;
-        localStorage.setItem("Score", score);
-        initialtime=5;
-        startTimer();
+        scorekeeper();
         displayquestions();
         rw.textContent="Correct!"
         setTimeout(function() {
             rw.textContent="";
         }, 3000);
     } else {
-        losses++;
         currentquestion++;
-        console.log(wins);
-        console.log(losses);
         startTimer();
         displayquestions();
         rw.textContent="Wrong!"
@@ -92,9 +113,51 @@ function guesscheck(answer) {
     }
 };
 
+ function generateform() {
+    
+        var init = document.createElement("input");
+        init.setAttribute=("type", "text");
+        var s = document.createElement("input");
+                s.setAttribute("type", "submit");
+                s.setAttribute("value", "Submit");
+        newform.appendChild(init);
+        newform.appendChild(s);
+    document.getElementsByTagName("body")[0]
+               .appendChild(newform);
+ };
+
+function scorekeeper () {
+
+    if (localStorage.score) {
+        localStorage.score = Number(localStorage.score)+Number(initialtime);
+      } else {
+        localStorage.score = Number(initialtime);
+}
+};
+
+function savehighscore() {
+    var finalscore = JSON.parse(localStorage.getItem("score"));
+    var highscore = {
+        // initials: comment.value.trim() make a form first
+        Hscore: finalscore
+      };
+};
+
+function retrievehighscore() {
+
+}
+
+
+function finalprompt() {
+    start.remove();
+    timerstart.remove();
+    generateform();
+}
+
 
 start.addEventListener("click", function() {
     startTimer();
     displayquestions();
+   
 });
 
